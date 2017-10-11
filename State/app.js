69bc631cb247e97afdb97d213866fdde20d8ddd0
Tylers7447 <tylers7447@gmail.com>
2017-10-11 02:01:38 -0500

    Update app.js

--- State/app.js
+++ State/app.js
@@ -1,11 +1,11 @@
 //app.js
-var routerApp = angular.module('routerApp', ['ui.router']);
-routerApp.config(funtion($stateProvider, $urlRouterProvider) {
-	$urlRouterProvider.otherwise('/home');
-	$stateProvider
+var routerApp = angular.module('routerApp', ['ui.router']);	//sets parameters for function call
+routerApp.config(funtion($stateProvider, $urlRouterProvider) {	//function call
+	$urlRouterProvider.otherwise('/home');			//initalize route and set default page
+	$stateProvider						//initialize states and where they are called from
 	
-	.state('home', {
-		url:'/home',
+	.state('home', {					//sets path for page and states where content is
+		url:'/home',					
 		templateUrl:'partial-home.html'
 	})
 	
